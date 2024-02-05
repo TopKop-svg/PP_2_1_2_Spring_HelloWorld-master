ecc18c098b636c0203e3bc1dd73a0c1e6d0edc75
TopKop-svg <loookintome@gmail.com>
2024-02-05 17:40:06 +0900

    create class Cat

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Cat {
     private String name;
 
@@ -7,5 +9,18 @@ public class Cat {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
